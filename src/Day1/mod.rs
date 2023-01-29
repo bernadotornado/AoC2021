0d48c136caffa3953650442f839b14d5bd008982
ADMIN\bszha <sze19728@spengergasse.at>
2023-01-29 11:38:25 +0100

    handle input

--- src/Day1/mod.rs
+++ src/Day1/mod.rs
@@ -2,5 +2,11 @@ use crate::Common::read_file;
 
 pub fn part_1() { 
     let contents = read_file();
-    println!("File contents (bytes): {:?}", contents);
+    // println!("File contents (bytes): {:?}", contents);
+    println!("Part 1");
+}
+pub fn part_2(){
+    let contents = read_file();
+    // println!("File contents (bytes): {:?}", contents);
+    println!("Part 2");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 
+use std::io::*;
 mod Day1;
 mod Common;
-use crate::Day1::part_1;
-use crate::Common::read_file;
 
 fn main() {
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
+    println!("You typed: {}", input);
+           match input.trim() {
+            "1_1" => Day1::part_1(),
+            "1_2" => Day1::part_2(),
+            _ => println!("Invalid input"),
+        }
+
     println!("Hello, world!");
-    part_1();
-    read_file();
+    Day1::part_1();
+    Common::read_file();
 }
